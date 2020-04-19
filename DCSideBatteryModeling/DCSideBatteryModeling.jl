5b611f3b89b95cb2cd3d5553da747298cea14b2d
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-18 20:58:16 -0700

    add PowerSystems.jl

--- DCSideBatteryModeling/DCSideBatteryModeling.jl
+++ DCSideBatteryModeling/DCSideBatteryModeling.jl
@@ -1,5 +1,11 @@
-using ModelingToolkit, OrdinaryDiffEq
+# Cant use import, see https://github.com/SciML/ModelingToolkit.jl/issues/319
+using ModelingToolkit
+import PowerSystems
+import OrdinaryDiffEq
+
 const MTK = ModelingToolkit
+const PSY = PowerSystems
+
 include("model.jl")
 #include("parameters.jl")
 #include("utils")
