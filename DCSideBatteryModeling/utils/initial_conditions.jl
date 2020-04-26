ec21f51a1a1779c100f2a250ba8a1c4979730d08
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-26 16:55:20 -0700

    enable use of Jacobian

--- DCSideBatteryModeling/utils/initial_conditions.jl
+++ DCSideBatteryModeling/utils/initial_conditions.jl
@@ -7,11 +7,10 @@ function solve_steady_state(initial_guess, parameter_values)
     _nl_system = MTK.NonlinearSystem(_eqs, [variables...], [params...][2:end])
     nlsys_func = MTK.generate_function(_nl_system, expression = Val{false})[2]
     _parameter_values = [x.second for x in parameter_values]
-    # Requires https://github.com/SciML/ModelingToolkit.jl/issues/323 to work
-    #nlsys_jac = MTK.generate_jacobian(_nl_system, expression = Val{false})[2] # second is in-place
+    nlsys_jac = MTK.generate_jacobian(_nl_system, expression = Val{false})[2] # second is in-place
     sol = NLsolve.nlsolve(
         (out, x) -> nlsys_func(out, x, _parameter_values),
-        #(out, x) -> nlsys_jac(out, x, _parameter_values),
+        (out, x) -> nlsys_jac(out, x, _parameter_values),
         initial_guess,
     )
     println(sol)
