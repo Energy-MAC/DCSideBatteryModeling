34bde60c19ce151c3e96eb58b85e742dc7381970
Jose Daniel Lara <jdlara@berkeley.edu>
2020-05-01 16:51:10 -0700

    update to main

--- main.jl
+++ main.jl
@@ -4,16 +4,35 @@ include(joinpath(pwd(), "DCSideBatteryModeling", "DCSideBatteryModeling.jl"))
 
 # Returns Generic ODE system
 model = get_model();
+f = MTK.generate_function(model)[2]
 ode_prob = instantiate_model(model, (0.0, 0.1))
 sol1 = solve(ode_prob, TRBDF2()); #Use Solver for stiff problems
 plot(sol1, vars = (0, 13), title = "DC Voltage Before Load Step")
 
+#_parameter_values = instantiate_parameters(model) #, system)
+#parameter_values = [x.second for x in _parameter_values]
+#jac = get_jacobian_function();
 
-# WIP functions
-_parameter_values = instantiate_parameters(model) #, system)
-parameter_values = [x.second for x in _parameter_values]
-jac = get_jacobian_function();
+# WIP Jacobian Experiments
+_, model_rhs, _, variables, params = get_internal_model(nothing)
+variable_count = length(variables)
+_eqs = zeros(length(model_rhs)) .~ model_rhs
+_nl_system = MTK.NonlinearSystem(_eqs, [variables...], [params...][2:end])
 
+# This works
+nlsys_jac = MTK.generate_jacobian(_nl_system)[2] # second is in-place
+jac = eval(nlsys_jac)
+param_eval = (out, params) -> jac(out, ode_prob.u0, params)
+n= length(ode_prob.u0)
+J = zeros(n, n)
+param_eval(J, parameter_values)
+
+# This causes StackOverflow works
+jac = MTK.generate_jacobian(_nl_system, expression = Val{false})[2] # second is in-place
+param_eval = (out, params) -> jac(out, ode_prob.u0, params)
+n= length(ode_prob.u0)
+J = zeros(n, n)
+param_eval(J, parameter_values)
 param_eval = (out, params) -> jac(out, ode_prob.u0, params)
 n= length(ode_prob.u0)
 J = zeros(n, n)
