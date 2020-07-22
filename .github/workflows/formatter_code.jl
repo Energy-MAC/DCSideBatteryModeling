2048fcb5d2743d8a413dab2180fdbf1320926618
Jose Daniel Lara <jdlara@berkeley.edu>
2020-07-22 13:35:39 -0700

    update formatter code

--- .github/workflows/formatter_code.jl
+++ .github/workflows/formatter_code.jl
@@ -1,11 +1,18 @@
 main_paths = ["."]
 for main_path in main_paths
-    format(
-        main_path;
-        whitespace_ops_in_indices = true,
-        remove_extra_newlines = true,
-        verbose = true,
-        always_for_in = true,
-        whitespace_typedefs = true,
-    )
+    for (root, dir, files) in walkdir(main_path)
+        for f in files
+            @show file_path = abspath(root, f)
+            !occursin(".jl", f) && continue
+            occursin("model_", file_path) && continue
+            format(
+                file_path;
+                whitespace_ops_in_indices = true,
+                remove_extra_newlines = true,
+                verbose = true,
+                always_for_in = true,
+                whitespace_typedefs = true,
+            )
+        end
+    end
 end
